3cbe305a47740a3ba65d5d81ef3f994f522115a6
aditya singh <adityasingh22849@gmail.com>
2025-08-24 20:28:52 +0530

    thanks

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 require('dotenv').config();
 const PORT =process.env.PORT || 3000;
-
+const person = require('./../models/person')
 const logRequest = (req,res,next)=>{
  console.log(`${new Date().toLocaleString()}`);
  next();
@@ -14,9 +14,13 @@ app.use(logRequest);
 app.get('/',(req,res)=>{
     res.send('WELCOME TO BACKEND');
 })
-const personRoutes = require('./router/personRoute');
-
-app.use('/person',personRoutes);
+// const personRoutes = require('./router/personRoute');
+app.get('/person',async (req,res)=>{
+   const data = await person.find();
+   console.log("data fetched")
+   res.json(data);
+})
+// app.use('/person',personRoutes);
 
 app.listen(PORT,()=>{
     console.log("LISTENING ON PORT 3000");
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const url = 'mongodb://127.0.0.1:27017/mydata'
+const url = 'mongodb://127.0.0.1:27017/mydata'
 // const url = 'mongodb+srv://adityasingh:aditya1234@cluster0.rgfetpl.mongodb.net/'
-const url = process.env.DB_URL;
+// const url = process.env.DB_URL;
 
 
 mongoose.connect(url,{
